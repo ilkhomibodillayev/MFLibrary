f4ec5a995b7cc8b0ecc6839f92d46c8022b4674d
ilkhomibodillayev <ilkhom89@gmail.com>
2022-04-19 12:28:43 +0500

    initial commit

---
+++ MFLibrary/MFLibrary/MFLibrary.h
@@ -0,0 +1,18 @@
+//
+//  MFLibrary.h
+//  MFLibrary
+//
+//  Created by Ilkhom Ziyadullaevich on 19/04/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MFLibrary.
+FOUNDATION_EXPORT double MFLibraryVersionNumber;
+
+//! Project version string for MFLibrary.
+FOUNDATION_EXPORT const unsigned char MFLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MFLibrary/PublicHeader.h>
+
+
